db1806fb4e7f428e86f5e9bb87b392f35b912e33
Nuxnuxx <nuxnux02290@gmail.com>
2022-06-12 18:32:43 +0200

    finish dijsktra

--- CalculcateurIti/module inout.cpp
+++ CalculcateurIti/module inout.cpp
@@ -23,6 +23,9 @@ sql::Connection* con;
 sql::PreparedStatement* pstmt;
 sql::ResultSet* result;
 
+/// <summary>
+/// methode de connexion a la bd et choix du schema
+/// </summary>
 void initBD() {
 	try
 	{
@@ -39,6 +42,12 @@ void initBD() {
 	con->setSchema("basec2");
 }
 
+/// <summary>
+///  methode qui permet de trouver le nouvel arret non visité avec le plus court temps de trajet
+/// </summary>
+/// <param name="dist">tableau des distances des arrets</param>
+/// <param name="visited">tableaux des arréts deja visité</param>
+/// <returns>numero d'arret qui n'a pas encore été visité avec le plus petit temps de trajet</returns>
 int minDistance(int dist[], bool visited[]) {
 	int minVal = inf;
 	int min;
@@ -52,6 +61,13 @@ int minDistance(int dist[], bool visited[]) {
 	return min;
 }
 
+/// <summary>
+/// implementation de l'algo de dijkstra
+/// </summary>
+/// <param name="matrice">tableau 2D comprenant la matrice d'adjacence avec les valeurs des distances</param>
+/// <param name="s">arret de depart</param>
+/// <param name="f">arret d'arrivée</param>
+/// <returns>le temps entre l'arret de depart et de fin</returns>
 int dijkstra(vector<vector<int>> matrice, int s, int f) {
 	int current;
 	int dist[V];
@@ -80,6 +96,11 @@ int dijkstra(vector<vector<int>> matrice, int s, int f) {
 	}
 }
 
+/// <summary>
+/// Methode qui rempli le nom des arrets dans le tableau path des arret qui sont utilisé pour le trajet demandé
+/// </summary>
+/// <param name="s">numero d'arret de depart</param>
+/// <param name="f">numero d'arret d'arrivée</param>
 void calcPath(int s, int f) {
 	int i;
 	path[0] = f;
@@ -92,12 +113,8 @@ void calcPath(int s, int f) {
 
 void initDijkstra() {
 
-	//int graph[V][V];
 	int source, final, time;
 	string sourceInput, finalInput;
-
-
-	//init graph
 	FILE* fichier = NULL;
 	char ch;
 	int num;
@@ -105,15 +122,19 @@ void initDijkstra() {
 	int i = 0;
 	int j = 0;
 	vector<vector<int>> graph(V, vector<int>(V));
+
+	// on rempli le graph de 0 puis on y implementera les durée si les deux arrets se rejoignent
 	for (int i = 0; i < V; i++) {
 		for (int j = 0; j < V; j++) {
 			graph[i][j] = 0;
 		}
 	}
+
+	// on ouvre la matrice et on lit caractere par caractére
 	fichier = fopen("M.txt", "r");
 	if (fichier == NULL)
 	{
-		printf("\nFichier impossible à ouvrir");
+		printf("\nFichier impossible a ouvrir");
 	}
 	else
 	{
@@ -138,7 +159,6 @@ void initDijkstra() {
 			}
 		}
 	}
-	
 
 	//initPrevious
 	for (int i = 0; i < V; i++) {
@@ -146,28 +166,36 @@ void initDijkstra() {
 	}
 
 	//user input
-	std::cout << "Saisir l'arrêt de départ : " << std::endl;
+	std::cout << "Saisir l'arret de depart : " << std::endl;
 	std::getline(std::cin, sourceInput);
-	std::cout << "Saisir l'arrêt d'arrivée : " << std::endl;
+	std::cout << "Saisir l'arret d'arrivee : " << std::endl;
 	std::getline(std::cin, finalInput);
 
-
 	//getnArret from user input
-	string request = "select nArret from Arret where nomArret = '" + sourceInput + "';";
-	pstmt = con->prepareStatement(request);
-	result = pstmt->executeQuery();
+	try {
+		string request = "select nArret from Arret where nomArret = '" + sourceInput + "';";
+		pstmt = con->prepareStatement(request);
+		result = pstmt->executeQuery();
 
-	while (result->next()) {
-		source = result->getInt("nArret");
+		while (result->next()) {
+			source = result->getInt("nArret");
+		}
+
+		string request1 = "select nArret from Arret where nomArret = '" + finalInput + "';";
+		pstmt = con->prepareStatement(request1);
+		result = pstmt->executeQuery();
+
+		while (result->next()) {
+			final = result->getInt("nArret");
+		}
+	}
+	catch (exception e) {
+		cout << e.what();
 	}
 
-	string request1 = "select nArret from Arret where nomArret = '" + finalInput + "';";
-	pstmt = con->prepareStatement(request1);
-	result = pstmt->executeQuery();
-
-	while (result->next()) {
-		final = result->getInt("nArret");
-	}
+	//numArret dans la base commence à 1 et à 0 dans l'algorithme donc on enleve 1 aux numéros d'arret 
+	final--;
+	source--;
 
 	//call algorithm
 	time = dijkstra(graph, source, final);
@@ -176,6 +204,7 @@ void initDijkstra() {
 	//print result
 	std::cout << "Il faut " << time << " minutes pour aller de " << sourceInput << " à " << finalInput << "." << std::endl;
 	std::cout << "Trajet : " << std::endl;
+
 	//BD trajet to do
 }
 
@@ -188,5 +217,5 @@ int main() {
 	delete con;
 	system("pause");
 	return 0;
-	
+
 }
