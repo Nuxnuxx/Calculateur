957ced9bfe8369ba660c12196f7c5e92bbaf865f
Florian <escande.florian.chatco@gmail.com>
2022-06-12 15:12:58 +0200

    sku

--- CalculcateurIti/module inout.cpp
+++ CalculcateurIti/module inout.cpp
@@ -7,6 +7,11 @@
 #include <fstream>
 using namespace std;
 
+const int inf = 999999; //représente l'infini
+const int V = 126; //nombre d'arrêts dans le réseau
+int path[V]; //chemin final
+int previous[V]; //prédécesseur du sommet
+
 const string server = "10.1.139.236";
 const string username = "c2";
 const string password = "mdp";
@@ -30,6 +35,95 @@ void initBD(){
     }
 
     con->setSchema("basec2");
+}
+
+int minDistance(int* dist, bool* visited) {
+	int minVal = inf;
+	int min;
+
+	for (int i = 0; i < V; i++) {
+		if (!visited[i] && dist[i] <= minVal) {
+			minVal = dist[i];
+			min = i;
+		}
+	}
+	return min;
+}
+
+int dijkstra(int** matrice, int s, int f) {
+	int current;
+	int dist[V];
+	bool visited[V];
+
+	for (int i = 0; i < V; i++) {
+		dist[i] = inf;
+		visited[i] = false;
+	}
+
+	dist[s] = 0;
+
+	for (int i = 0; i < V - 1; i++) {
+		current = minDistance(dist, visited);
+		if (current == f) {
+			return dist[f];
+		}
+		visited[current] = true;
+
+		for (int j = 0; j < V; j++) {
+			if (!visited[j] && matrice[current][j] > 0 && dist[current] != inf && (dist[current] + matrice[current][j] < dist[j])) {
+				dist[j] = dist[current] + matrice[current][j];
+				previous[j] = current;
+			}
+		}
+	}
+}
+
+void calcPath(int s, int f) {
+	int i;
+	path[0] = f;
+	i = 1;
+	while (path[i] != s) {
+		path[i] = previous[path[i - 1]];
+		i++;
+	}
+}
+
+void initDijkstra() {
+	int graph[V][V];
+	int source, final, time;
+	char* sourceInput, finalInput;
+
+	ofstream fichier("M.txt");
+	if (fichier)
+	{
+		for (int i = 0; i < 126; i++) {
+			for (int j = 0; j < 126; j++) {
+				fichier << M[i][j] << " ";
+			}
+			fichier << endl;
+		}
+		fichier.close();
+	}
+	else
+		cout << "Impossible d'ouvrir le fichier !" << endl;
+
+	for (int i = 0; i < V; i++) {
+		previous[i] = NULL;
+	}
+
+	std::cout << "Saisir l'arrêt de départ : " << std::endl;
+	std::cin >> sourceInput;
+	std::cout << "Saisir l'arrêt d'arrivée : " << std::endl;
+	std::cin >> finalInput;
+
+	//BD assigne narret
+
+	time = dijkstra(graph, source, final);
+	calcPath(source, final);
+
+	std::cout << "Il faut " << time << " minutes pour aller de " << sourceInput << " à " << finalInput << "." << std::endl;
+	std::cout << "Trajet : " << std::endl;
+	//BD
 }
 
 int main() {
